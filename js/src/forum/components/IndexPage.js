e5ad635a17acb6a0d7e86c749fa594b805e962b1
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-08 11:56:47 -0400

    Removed unnecessary / incorrect comment

--- js/src/forum/components/IndexPage.js
+++ js/src/forum/components/IndexPage.js
@@ -51,7 +51,6 @@ export default class IndexPage extends Page {
       });
     }
 
-    // If we just cleared the discussion list, reset the params.
     if (!app.cache.discussionList.hasDiscussions()) {
       app.cache.discussionList.setParams(params);
       app.cache.discussionList.refresh();
