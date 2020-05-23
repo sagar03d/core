becc47858ecc3528e8fe0bdd21bf77d2d53f4b21
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-23 04:42:39 -0400

    Encapsulate app.current, app.previous via PageState

--- js/src/forum/components/DiscussionPage.js
+++ js/src/forum/components/DiscussionPage.js
@@ -42,7 +42,7 @@ export default class DiscussionPage extends Page {
       app.pane.enable();
       app.pane.hide();
 
-      if (subclassOf(app.previous, DiscussionPage)) {
+      if (app.previous && app.previous.subclassOf(DiscussionPage)) {
         m.redraw.strategy('diff');
       }
     }
@@ -201,10 +201,10 @@ export default class DiscussionPage extends Page {
     this.stream.on('positionChanged', this.positionChanged.bind(this));
     this.stream.goToNumber(m.route.param('near') || (includedPosts[0] && includedPosts[0].number()), true);
 
-    app.currentData = {
+    app.current.setData({
       discussion: discussion,
       stream: this.stream,
-    };
+    });
   }
 
   /**
